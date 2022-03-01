0c39a735726e0a2eb35cf4e27b7f26bd1cba6426
Michał Maciejewski <maciejewskimichal7@gmail.com>
2022-03-01 01:45:15 +0100

    clearing and change addOffer validation msg in schema

--- src/organisms/addOfferForm/addOffer.schema.ts
+++ src/organisms/addOfferForm/addOffer.schema.ts
@@ -4,18 +4,18 @@ export const addOfferSchema = Yup.object().shape({
   title: Yup.string()
     .min(5, "tytuł jest za krótki")
     .max(50, "tytuł jest za długi")
-    .required("Required"),
+    .required("Podaj tytuł"),
   description: Yup.string()
     .min(20, "Opis jest za krótki")
     .max(1000, "Opis jest za długi")
-    .required("Required"),
+    .required("Opis jest wymagany"),
   telephone: Yup.string()
     .min(9, "Numer jest za krótki")
     .max(9, "Numer jest za długi")
-    .required("Required"),
+    .required("Podaj telefon"),
   confirmTelephone: Yup.string()
     .oneOf([Yup.ref('telephone'), null], "Numery telefonów nie są takie same")
-    .required("Required"),
+    .required("Potwierdz telefon"),
 //   contactName: Yup.string()
 //     .min(2, "Too Short!")
 //     .max(50, "Too Long!")
@@ -25,5 +25,5 @@ export const addOfferSchema = Yup.object().shape({
   city: Yup.string()
     .min(2, "Nazwa miasta jest za krótka")
     .max(50, "Nazwa miasta jest za długa")
-    .required("Required"),
+    .required("Podaj miasto"),
 });
--- src/services/addOffer.types.ts
+++ src/services/addOffer.types.ts
@@ -25,11 +25,3 @@ export type AddOfferFormData = Pick<
   | "freeSpaceFrom"
   | "freeSpaceTo"
 >;
-
-categoryId: "81b84c69-1c9a-418f-8538-66e47041a65a";
-city: "asdfasdfasdf";
-contactName: "na razie na sztywno ;>";
-description: "asdffdscqwerwercqqwevr hdg wy53 sdfg";
-telephone: "123123123";
-title: "asdfasdfdfas123";
-voivodeship: "2";
